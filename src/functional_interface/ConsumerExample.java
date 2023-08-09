02ab95705aa2d3fc7ad8f6b7173941b339975d7a
Guilherme Guedes Domingues <guilhermedomingues@icloud.com>
2023-08-09 12:23:27 -0300

    Functional Interface Consumer

---
+++ src/functional_interface/ConsumerExample.java
@@ -0,0 +1,26 @@
+package functional_interface;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ConsumerExample {
+  public static void main(String[] args) {
+    // Criar uma lista de números inteiros
+    List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+    // Usar o Consumer com expressão lambda para imprimir números pares
+    // Consumer<Integer> imprimirNumeroPar = numero -> {
+    //   if (numero % 2 == 0) {
+    //     System.out.println(numero + " ");
+    //   }
+    // };
+
+    // Usar o Consumer para imprimir números pares no Stream
+    // numeros.stream().forEach(imprimirNumeroPar);
+    numeros.stream().forEach(t -> {
+        if (t % 2 == 0) {
+            System.out.println(t);
+        }
+    });
+  }
+}
