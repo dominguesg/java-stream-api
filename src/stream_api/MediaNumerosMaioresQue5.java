e6303c5b182c0d2d141a155229afe8ef13db9b74
Guilherme Guedes Domingues <guilhermedomingues@icloud.com>
2023-08-10 19:57:25 -0300

    Conclusão dos desafios de Stream API

--- src/stream_api/MediaNumerosMaioresQue5.java
+++ src/stream_api/MediaNumerosMaioresQue5.java
@@ -8,12 +8,17 @@ public class MediaNumerosMaioresQue5 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        
-
         System.out.println("A média dos valores maiores que 5 é: ");
-        numeros.stream()
+        // List<Integer> numerosMaioresQue5 = numeros.stream()
+        //         .filter(n -> n > 5)
+        //         .collect(Collectors.toList());
+
+                numeros.stream()
                 .filter(n -> n > 5)
-                .collect(Collectors.averagingDouble(Double::doubleValue))
-                .average();
+                .collect(Collectors.toList())
+                .stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .ifPresent(System.out::println);
     }
 }
